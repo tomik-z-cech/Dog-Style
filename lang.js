437d4dcbf1e018e4f71f183ddee46c909837e66d
tomik-z-cech <tkubancik@gmail.com>
2023-11-09 22:48:35 +0000

    Move lang.js to root

--- lang.js
+++ lang.js
@@ -3,10 +3,10 @@ let lang = navigator.language || navigator.userLanguage;
 // Load the appropriate HTML file based on the user's language preference
 console.log(lang)
 if (lang === 'en') {
-    window.location.href = '/index_en.html';
+    window.location.href = 'index_en.html';
 } else if (lang === 'pl') {
-    window.location.href = '/index_pl.html';
+    window.location.href = 'index_pl.html';
 } else {
     // Default to English or any other fallback language
-    window.location.href = '/index_en.html';
+    window.location.href = 'index_en.html';
 }
