88fb1debae861187b1dc7ad6abf50c9e950f0aa9
tomik-z-cech <tkubancik@gmail.com>
2024-01-11 15:42:56 +0000

    landing page - django

--- landing/views.py
+++ landing/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from django.views import generic
 
 # Create your views here.
+class LandingPageView(generic.ListView):
+    """
+    Class generates view landing page
+    """
+
+    template_name = "landing/index.html"
+
+    def get(self, request, *args, **kwargs):
+        """This method generates view of landing page"""
+        return render(
+            request,
+            self.template_name,
+            {
+            },
+        )
